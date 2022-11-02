e3a5d37a8558cff4f4b73f4e11ff07bca46fb369
Marco Stefancich <mstefancich@gmail.com>
2022-11-02 01:40:04 +0100

    SW 1.2

--- Spare_parts/sw_1.js
+++ Spare_parts/sw_1.js
@@ -11,12 +11,3 @@ self.addEventListener("install", e => {
     );
     console.log("Resources in cache. Done");
 })
-
-self.addEventListener("fetch", e => {
-    console.log('intercepting fecth request for:', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    )
-});
